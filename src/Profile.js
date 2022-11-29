5891393ba54cbcfc93c2d37a18d25e7991352f53
Gabrielle Rogers <gabirogers23@gmail.com>
2022-11-28 19:40:43 -0600

    First Commit

---
+++ src/Profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <div className="name">Name:</div>
+      <div className= "email">Email:</div>
+      <div className = "address">Address:</div>
+      <div className = "photo">Photo</div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Profile from './Profile';
+
+const App = (props) => {
+  return <div className="app">
+    <Profile />
+    <div className="dark-mode">
+      <h1>Dark Mode</h1>
+    </div>
+    <div className="tic-tac-toe">
+      <h1>Tic-Tac-Toe</h1>
+    </div>
+    <div className="todo">
+      <h1>Todo</h1>
+    </div>
+  </div>
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
